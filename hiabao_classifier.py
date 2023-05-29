979e9b610941bf4251fe80e70aa6b54248de739a
Lucasticc <lucasprint@163.com>
2023-05-29 10:16:39 +0800

    对图像处理的更改

--- hiabao_classifier.py
+++ hiabao_classifier.py
@@ -9,8 +9,10 @@ class classifier():
         self.model.eval
 
     def upload(self,pic):
+        # face = cv2.imread('pic')
         face = cv2.resize(pic, (128, 128), interpolation=cv2.INTER_CUBIC)
-        face_normalized = face.reshape(3, 128, 128) / 255.0 # 为与pytorch中卷积神经网络API的设计相适配，需reshape原图
+        # face_normalized = face.reshape(3, 128, 128) / 255.0 # 为与pytorch中卷积神经网络API的设计相适配，需reshape原图
+        face_normalized=face.transpose((2, 0, 1))/255.0
         face_tensor = torch.from_numpy(face_normalized) # 将python中的numpy数据类型转化为pytorch中的tensor数据类型
         face_tensor = face_tensor.type('torch.Tensor') # 指定为'torch.FloatTensor'型，否则送进模型后会因数据类型不匹配而报错
         face_tensor = face_tensor.unsqueeze(0)
